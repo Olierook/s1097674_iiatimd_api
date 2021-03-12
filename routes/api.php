318ab1cbf19bf7537091082dbbcbe3b099c0a336
Olierook <Robbert_olierook@hotmail.com>
2021-03-12 15:57:07 +0100

    cleanup

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+
 use App\Camper;
 
 
@@ -28,65 +26,3 @@ Route::get('campers/{camper}', 'CamperController@show');
 Route::post('campers', 'CamperController@store');
 Route::put('campers/{camper}', 'CamperController@update');
 Route::delete('campers/{camper}', 'CamperController@delete');
-
-// Route::group([
-//
-//     'middleware' => 'api',
-//     'prefix' => 'auth'
-//
-// ], function ($router) {
-//
-//     Route::post('login', 'AuthController@login');
-//     Route::post('logout', 'AuthController@logout');
-//     Route::post('refresh', 'AuthController@refresh');
-//     Route::post('me', 'AuthController@me');
-//
-// });
-
-
-//
-// Route::post('login', 'ApiController@login');
-// Route::post('register', 'ApiController@register');
-
-
-// Route::group(['middleware' => 'auth:api'], function() {
-
-
-
-// Route::group(['middleware' => 'auth.jwt'], function () {
-//
-//     Route::get('logout', 'ApiController@logout');
-//     Route::get('user', 'ApiController@getAuthUser');
-//
-//     Route::get('campers', 'CamperController@index');
-//     Route::get('campers/{camper}', 'CamperController@show');
-//     Route::post('campers', 'CamperController@store');
-//     Route::put('campers/{camper}', 'CamperController@update');
-//     Route::delete('campers/{camper}', 'CamperController@delete');
-// });
-
-
-// // Route::group(['middleware' => 'auth:api'], function() {
-//     Route::get('campers', 'CamperController@index');
-//     Route::get('campers/{camper}', 'CamperController@show');
-//     Route::post('campers', 'CamperController@store');
-//     Route::put('campers/{camper}', 'CamperController@update');
-//     Route::delete('campers/{camper}', 'CamperController@delete');
-// // });
-
-// Route::get('campers', 'CamperController@index');
-// Route::get('campers/{camper}', 'CamperController@show');
-// Route::post('campers', 'CamperController@store');
-// Route::put('campers/{camper}', 'CamperController@update');
-// Route::delete('campers/{camper}', 'CamperController@delete');
-
-// Route::post('register', 'Auth\RegisterController@register');
-// Route::post('register', 'Auth\RegisterController@register');
-// Route::post('login', [ 'as' => 'login', 'uses' => 'Auth\LoginController@login']);
-// Route::middleware('auth:api')
-//     ->get('/user', function (Request $request) {
-//         return $request->user();
-//     });
-// Auth::guard('api')->user();
-// Auth::guard('api')->check();
-// Auth::guard('api')->id();
